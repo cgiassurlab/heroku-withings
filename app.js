feead30b617a35994785ba53f365f87a6acda906
matcgi <mathieu.leprevost@cgi.com>
2015-11-19 16:18:50 +0100

    nouvelle version6

--- app.js
+++ app.js
@@ -268,7 +268,7 @@ app.get('/withings/activity/steps', function (req, res) {
 
 // Display today's steps for a user
 app.get('/withings/activity/weight', function (req, res) {
-	document.getElementById("MyEdit").innerHTML = "My new text!";​
+	$("#MyEdit").html("My new text!");
 	res.sendFile(path.join(__dirname + '/weight.html'));
 	
 
