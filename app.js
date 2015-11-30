f54143829d5eada2721a11fab51d5b07b0a2f6d6
lauraSouchu <laura.souchu@cgi.com>
2015-11-30 09:58:42 +0100

    news

--- app.js
+++ app.js
@@ -269,7 +269,7 @@ app.get('/withings/activity/steps', function (req, res) {
 
 
 // Display today's steps for a user
-app.get('/withings/activity/weight', express.timeout(5000), express.bodyParser(), function (req, res) {
+app.get('/withings/activity/weight', express.timeout(6000), express.bodyParser(), function (req, res) {
 	if (res._header) return; // someone already responded
 	var timedout = false;
 	req.on('timeout',function(){
