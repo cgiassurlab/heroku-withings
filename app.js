28ce5c19806271f9197536949bc78bbbae76202e
henaudb <benoit.henaud@cgi.com>
2015-12-17 11:09:19 +0100

    a

--- app.js
+++ app.js
@@ -384,6 +384,33 @@ app.get('/withings/api/myday', function (req, res) {
 
     console.log("UserId : "+gUserID);
 
-    var data = {}
+    var data = 
+
+{
+    "glossary": {
+        "title": "example glossary",
+    "GlossDiv": {
+            "title": "S",
+      "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+          "SortAs": "SGML",
+          "GlossTerm": "Standard Generalized Markup Language",
+          "Acronym": "SGML",
+          "Abbrev": "ISO 8879:1986",
+          "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+            "GlossSeeAlso": ["GML", "XML"]
+                    },
+          "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
+
+
+
     res.json(data);
 });
